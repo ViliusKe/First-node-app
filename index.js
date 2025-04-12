9ae88a14b5fa3187b6198cc9ecc0c84dd140b7eb
ViliusKe <kerysvilius@gmail.com>
2025-04-12 16:29:34 +0300

    small touch up

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.use((req, res) => {
   return res.status(404).json({ message: "Endpoint does not exist" });
 });
 
-// const port = 3000;
-
 app.listen(process.env.PORT, () => {
   console.log(`APP STARTED ON PORT ${process.env.PORT}`);
 });
